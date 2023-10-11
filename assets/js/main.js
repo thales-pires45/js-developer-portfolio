74219901cf1d0c6322ec51c52e6c5a9bab87cc92
Thales Pires <thalespires45@hotmail.com>
2023-10-11 17:27:18 -0300

    Idiomas Carregados

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,11 +35,17 @@ function updateHardSkills(profileData) {
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"></li>`).join('')
 }
 
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
 //Pega os fetch do doc
 (async ()  => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
-    console(profileData)
+    updateLanguages(profileData)
+    console.log(profileData)
 })()
